b135103b3519c38bdc12cb60ae6ddf28cf595cad
Mohammed Mouafik <79029693+mouafikmohammed@users.noreply.github.com>
2022-10-03 15:15:28 +0100

    Empty

--- date.php
+++ date.php
@@ -1,17 +1,4 @@
 <?php
-$exp_date= date(Ymd,strtotime('+15 day'));
-$today_date=date(Ymd);
 
- now convert to strtotime
-$exp=strtotime($exp_date);
-$td=strtotime($today_date);
-
-
-if($td$exp){
-    echo expired;
-}
-else{
-    echo not expired;
-}
 
 ?>
