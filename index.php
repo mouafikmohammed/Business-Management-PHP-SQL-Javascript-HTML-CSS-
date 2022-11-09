4d508ea0937ae2f6e967424dcf1aef80c29b0311
Mohammed Mouafik <79029693+mouafikmohammed@users.noreply.github.com>
2022-11-09 08:01:07 +0100

    Dashboard

--- index.php
+++ index.php
@@ -49,6 +49,86 @@ session_start();
          <!------------------------------------------------------->
          <div class="weatherWidget"></div> <!--this div for weather-->
          <!--write here the Dashboard code-->
+         <main>
+				<div class="cards">
+					
+					<div class="card-single">
+						<img src="img/sales.png" width="30" height="30">
+						<div>
+							<?php
+								require 'connection.php';
+								$query = "SELECT * FROM sells";  
+								$query_run = mysqli_query($con, $query);
+
+								# $row = mysqli_num_rows($query_run);
+								$row = mysqli_fetch_array($query_run);
+
+								$tot = $row['price']*$row['quantity'];
+								while($row = mysqli_fetch_array($query_run)){
+									$tot1 = $tot + $row['price']*$row['quantity'];
+								}
+								echo '<h4> total sales: '.$tot1.'$</h4>';
+							?>
+							<small>sales</small>
+						</div>
+						<div>
+							<span class="fa fa-shopping-cart"></span>
+						</div>
+					</div>
+
+					<div class="card-single">
+					<img src="img/stock.png" width="30" height="30">
+						<div>
+							<?php
+								require 'connection.php';
+								$query = "SELECT quantity  FROM purchase ORDER BY quantity";  
+								$query_run = mysqli_query($con, $query);
+								$row = mysqli_num_rows($query_run);
+								echo '<h4> total in stock: '.$row.'</h4>';
+							?>
+							<small>Stock</small>
+						</div>
+						<div>
+							<span class="fa fa-newspaper-o"></span>
+						</div>
+					</div>
+
+					<div class="card-single">
+						<img src="img/purshases.png" width="30" height="30">
+						<div>
+							<?php
+								require 'connection.php';
+								$query = "SELECT * FROM purchase";
+								$query_run = mysqli_query($con, $query);
+
+								$row = mysqli_fetch_array($query_run);
+
+								$purchase = $row['price']*$row['quantity'];
+								while($row = mysqli_fetch_array($query_run)){
+									$purchase1 = $purchase + $row['price']*$row['quantity'];
+								}
+								echo '<h4> total purchases: '.$purchase1.'$</h4>';
+							?>
+							<small>purchase</small>
+						</div>
+					</div>
+					<div class="card-single">
+						<img src="img/purshases.png" width="30" height="30">
+						<div>
+							<?php
+								require 'connection.php';
+								$exp_date= date("Y-m-d",strtotime('-7 day'));
+								$query = "SELECT * FROM purchase WHERE DATE BETWEEN '2000-07-02' and '$exp_date'";
+
+								$query_run = mysqli_query($con, $query);
+								$row1 = mysqli_num_rows($query_run);
+								echo '<h4> Expired Products: '.$row1.'</h4>';
+							?>
+							<small>purchase</small>
+						</div>
+					</div>
+				</div>
+			</main>
       </div>
 </body>
 </html>
