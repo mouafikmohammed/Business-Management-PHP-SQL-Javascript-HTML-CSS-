c1e6f69deccdbd22eb299ba7fbce553036a0ffdc
Mohammed Mouafik <79029693+mouafikmohammed@users.noreply.github.com>
2022-11-12 09:40:10 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@ $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 $mail->Username   = 'mouafikmohammed03@gmail.com';              //SMTP username
-$mail->Password   = 'nwlovsbsackdqups';                           //SMTP password
+$mail->Password   = 'my secret code';                           //SMTP password
 $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
